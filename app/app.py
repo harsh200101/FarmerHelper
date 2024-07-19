b18af0ecece236f01874952099421cb824c38e41
Harsh Gajbhiye <140972242+harsh200101@users.noreply.github.com>
2024-07-19 22:36:29 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -70,7 +70,7 @@ disease_model.eval()
 # Loading crop recommendation model
 
 
-crop_recommendation_model_path = 'models\RandomForest.pkl'
+crop_recommendation_model_path = 'models/RandomForest.pkl'
 crop_recommendation_model = pickle.load(open(crop_recommendation_model_path, 'rb'))
 
 
